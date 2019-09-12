0cf596499184ab1a82b11e23971934420ce5bfb6
hashika <m.g.hashikamaduranga@gmail.com>
2019-09-12 20:56:34 +0530

    practice java script

---
+++ jsPage/app.js
@@ -0,0 +1,51 @@
+// //documen.getElementbyId()
+
+// // console.log(document.getElementById('task-title').style.color="black");
+// // //console.log(document.getElementById('task-title').style.display="none");
+
+// // document.getElementById("task-title").textContent="hashika";
+// // document.getElementById('task-title').innerHTML='<span style="color:red"></span>';
+
+// //document.quertSelector()
+
+// // console.log(document.querySelector('#task-title').getElementsByClassName.color="red");
+
+// // console.log(document.querySelector('h5'));
+
+// // document.querySelector('li').style.color='red';
+// // document.querySelector('li:last-child').style.color='red';
+// // document.querySelector('li:nth-child(2)').style.color='blue';
+
+// //documnet.getElementByClassName
+
+
+// console.log(document.getElementsByClassName('collection-item'));
+
+// document.querySelector('ul').getElementsByClassName('collecton-item');
+// const list=document.getElementsByClassName('collection-item');
+
+
+// const li=document.getElementsByTagName('li');
+// console.log(li[0]);
+
+// //convert HTML Collection into array
+
+// // li=Array.from(li);
+// // li.reverse();
+
+// // li.forEach(function(li,index){
+// //     console.log(li.className);
+// // })
+
+// const items=document.querySelectorAll('ul.collection li.collection-item');
+
+// items.forEach(function(item,index){
+//     item.textContent='${index}: Hello';
+// });
+// console.log(items);
+
+// const liOdd=document.querySelectorAll('li:nth-child(odd)');
+
+// liOdd.forEach(function(li,index){
+//     li.style.background='#ccc';
+// })
